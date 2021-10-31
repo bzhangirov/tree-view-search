267c815536e3505a1e9161ca21d675007f89f415
Bolatbek Zhangirov <bzhangirov@beeline.kz>
2021-10-31 12:55:44 +0600

    tree view search

---
+++ src/components/TreeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    {{ getBrands }}
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  computed: {
+    ...mapGetters('treeSearch', [
+        'getBrands'
+    ])
+  },
+  created() {
+    this.getAllBrands()
+  },
+  methods: {
+    ...mapActions('treeSearch', [
+      'getAllBrands'
+    ])
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
